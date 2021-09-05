ba69fd3357c74721c06578948a712a44993eed0e
Brian Gamble <brianjgamble@users.noreply.github.com>
2021-09-05 18:25:21 -0500

    Draw and move a circle left/right with 'A' and 'D' keys

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
 #include <raylib.h>
 
 int main() {
-    InitWindow(350, 200, "First Window!");
+    int width{350};
+    int height{200};
+    InitWindow(width, height, "First Window!");
+
+    int circle_x{175};
+    int circle_y{100};
+
+    SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(WHITE);
+
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+
+        if (IsKeyDown(KEY_D) && circle_x < width) {
+            circle_x += 10;
+        } else if (IsKeyDown(KEY_A) && circle_x > 0) {
+            circle_x -= 10;
+        }
+
         EndDrawing();
     }
 
