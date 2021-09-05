197f63e4e1d2b94aa4120965a01eebd6c344a304
Brian Gamble <brianjgamble@users.noreply.github.com>
2021-09-05 16:18:03 -0500

    Close the window

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,12 @@
 
 int main() {
     InitWindow(350, 200, "First Window!");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RED);
+        EndDrawing();
+    }
+
     return 0;
 }
