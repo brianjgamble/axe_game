1550281fe35bba625ce636b95db786d1eeecf108
Brian Gamble <brianjgamble@users.noreply.github.com>
2021-09-05 19:33:16 -0500

    Add collision detection

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-# cpp_starter_project
+# Axe Game
 
-This is a collection of files to kickoff a new C++ project.
-
-*Remember to update [CMakeLists.txt](CMakeLists.txt) and change the name to your new project.*
-
-## Links
-- [Catch2](https://github.com/catchorg/Catch2) - C++ testing framework
-- [Conan](https://conan.io) - C/C++ Package Manager
-- [CMake](https://cmake.org) - Suite of tools to build, test and package software
+This is the first project from the course [C++ Fundamentals: Game Programming for Beginners](https://www.gamedev.tv/courses/1216775)
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,34 @@
 #include <raylib.h>
 
-int main() {
+int main()
+{
     int width{800};
     int height{450};
-    InitWindow(width, height, "First Window!");
+    InitWindow(width, height, "Axe Game");
 
     int circle_x{200};
     int circle_y{200};
+    int circle_radius{25};
+    int l_circle_x{circle_x - circle_radius};
+    int r_circle_x{circle_x + circle_radius};
+    int u_circle_y{circle_y - circle_radius};
+    int b_circle_y{circle_y + circle_radius};
 
     int axe_x{400};
     int axe_y{};
+    int axe_length{50};
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x + axe_length};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y + axe_length};
 
     int direction{10};
+
+    bool collision_with_axe =
+        (b_axe_y >= u_circle_y) &&
+        (u_axe_y <= b_circle_y) &&
+        (r_axe_x >= l_circle_x) &&
+        (l_axe_x <= r_circle_x);
 
     SetTargetFPS(60);
 
@@ -19,20 +36,46 @@ int main() {
         BeginDrawing();
         ClearBackground(WHITE);
 
-        DrawCircle(circle_x, circle_y, 25, BLUE);
-        DrawRectangle(axe_x, axe_y, 50, 50, RED);
-
-        axe_y += direction;
-        if (axe_y > height || axe_y < 0) {
-            direction = -direction;
+        if (collision_with_axe) {
+            DrawText("Game Over!", 400, 200, 20, RED);
         }
+        else {
+            // Game logic begins
 
-        if (IsKeyDown(KEY_D) && circle_x < width) {
-            circle_x += 10;
-        } else if (IsKeyDown(KEY_A) && circle_x > 0) {
-            circle_x -= 10;
+            // update the edges
+            l_circle_x = circle_x - circle_radius;
+            r_circle_x = circle_x + circle_radius;
+            u_circle_y = circle_y - circle_radius;
+            b_circle_y = circle_y + circle_radius;
+            l_axe_x = axe_x;
+            r_axe_x = axe_x + axe_length;
+            u_axe_y = axe_y;
+            b_axe_y = axe_y + axe_length;
+
+            // update collision with axe
+            collision_with_axe =
+                (b_axe_y >= u_circle_y) &&
+                (u_axe_y <= b_circle_y) &&
+                (r_axe_x >= l_circle_x) &&
+                (l_axe_x <= r_circle_x);
+
+            DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+            DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
+
+            axe_y += direction;
+            if (axe_y > height || axe_y < 0) {
+                direction = -direction;
+            }
+
+            if (IsKeyDown(KEY_D) && circle_x < width) {
+                circle_x += 10;
+            }
+            else if (IsKeyDown(KEY_A) && circle_x > 0) {
+                circle_x -= 10;
+            }
+
+            // Game logic ends
         }
-
         EndDrawing();
     }
 
